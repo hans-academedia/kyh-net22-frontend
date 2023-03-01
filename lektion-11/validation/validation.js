6c6a50cdb725c001aff31e3d6a597f4dd8694ba5
Hans Mattin-Lassei <hans.mattin-lassei@epndata.com>
2023-03-01 15:25:23 +0100

    Lektion 11 (Form validation)

--- lektion-11/validation/validation.js
+++ lektion-11/validation/validation.js
@@ -1,4 +1,4 @@
-const toggu017FlePassword = () => {
+const togglePassword = () => {
     const password = document.getElementById('password')
     const icon = document.getElementById('toggle-password-icon')
 
@@ -14,41 +14,80 @@ const toggu017FlePassword = () => {
 
 const handleSubmit = (e) => {
     e.preventDefault()
+    const errors = []
+
+    for(let element of e.target) {
+        if (element.required) {
+            switch(element.type) {
+                case 'email':
+                    if (!validateEmail(element.value))
+                        errors.push(element)
+                    break;
+                case 'text':
+                    if (!validateName(element.value))
+                        errors.push(element)
+                    break;
+                case 'password':
+                    if (!validatePassword(element.value))
+                        errors.push(element)
+                    break; 
+            }
+        }
+    }
+
+    if(errors.length == 0)
+        window.location.reload('success.html')
 }
 
-const validateName = (e) => {
-    e.target.value = e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
+const validateName = (value) => {
+    // e.target.value = e.target.value.charAt(0).toUpperCase() + e.target.value.slice(1)
 
-    const regEx = /^[A-Ö]([a-öA-Ö\u00C0-\u017F]+(([' -][a-öA-Ö])?[a-öA-Ö]*)){1,}$/
-    if (!regEx.test(e.target.value)) {
-        console.log("firstname not valid")
+    const regEx = /^([a-öA-Ö\u00C0-\u017F]+(([' -][a-öA-Ö])?[a-öA-Ö]*)){2,}$/
+    if (!regEx.test(value)) {
         return false
     }
-        
-    console.log("firstname valid")
+       
+    console.log("name valid")
     return true
+
+
+    // if(value.length < 2)
+    //     return false
+    
+    // return true
 }
 
-const validateEmail = (e) => {
+const validateEmail = (value) => {
     const regEx = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
 
-    if (!regEx.test(e.target.value)) {
-        console.log("email not valid")
+    if (!regEx.test(value)) {
         return false
     }
         
-    console.log("email valid")
     return true
 }
 
-const validatePassword = (e) => {
+const validatePassword = (value) => {
     const regEx = /^(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/;
 
-    if (!regEx.test(e.target.value)) {
-        console.log("password not valid")
+    if (!regEx.test(value)) {
         return false
     }
         
-    console.log("password valid")
     return true
+}
+
+
+const validate = (e) => {    
+    switch(e.target.type) {
+        case 'email':
+            validateEmail(e.target.value)
+            break;
+        case 'text':
+            validateName(e.target.value)
+            break;
+        case 'password':
+            validatePassword(e.target.value)
+            break;
+    }
 }
